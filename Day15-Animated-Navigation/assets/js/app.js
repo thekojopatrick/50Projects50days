7a89fa468c72e6a9b2af5b74cefd97b3e374fdf9
Kojo Patrick <patrickasare84@gmail.com>
2021-06-14 03:23:37 +0000

    add animated navbar

---
+++ Day15-Animated-Navigation/assets/js/app.js
@@ -0,0 +1,6 @@
+const nav =  document.getElementById('nav')
+const toggle = document.getElementById('toggle')
+
+toggle.addEventListener('click',function () {
+    nav.classList.toggle('active')
+})
