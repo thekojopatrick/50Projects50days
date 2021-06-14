5361546ef7ba91678eed51ec6fc70a67561c90c8
Kojo Patrick <patrickasare84@gmail.com>
2021-06-14 03:25:39 +0000

    format code

--- Day15-Animated-Navigation/assets/js/app.js
+++ Day15-Animated-Navigation/assets/js/app.js
@@ -1,6 +1,6 @@
-const nav =  document.getElementById('nav')
+const nav = document.getElementById('nav')
 const toggle = document.getElementById('toggle')
 
-toggle.addEventListener('click',function () {
-    nav.classList.toggle('active')
+toggle.addEventListener('click', function () {
+	nav.classList.toggle('active')
 })
